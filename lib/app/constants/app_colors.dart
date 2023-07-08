b13d979795819ae09502d882b940e7f3724f5983
Manil Pun <punmanil3@gmail.com>
2023-07-08 12:12:00 +0545

    app colors

---
+++ lib/app/constants/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const kBackgroundColor = Color(0xFFF6F6F6);
+  static const kBlackColor = Color(0xFF231716);
+  static const kGreyColor = Color(0xFF74797D);
+  static const kRedColor = Color(0xFFE3001B);
+  static const kTextColorLight = Color(0xFFA5A5A5);
+
+  static const kWebDarkColor = Color(0xFF1A1A27);
+  static const kWebLightColor = Color(0xFF1E1E2D);
+  static const kWebGreyColor = Color(0XFF494B74);
+}
