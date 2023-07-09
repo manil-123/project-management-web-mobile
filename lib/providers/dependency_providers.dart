2fe0de316403189f9ce26b69b611c4d8b0b7104f
Manil Pun <punmanil3@gmail.com>
2023-07-09 12:12:00 +0545

    added secure storage and userdao provider

--- lib/providers/dependency_providers.dart
+++ lib/providers/dependency_providers.dart
@@ -1,7 +1,18 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:project_management_web_and_mobile/api/dio_config.dart';
 import 'package:project_management_web_and_mobile/api/rest_client.dart';
+import 'package:project_management_web_and_mobile/app/database/secure_storage.dart';
+import 'package:project_management_web_and_mobile/app/database/user_details_dao.dart';
 
 final restClientProvider = Provider<RestClient>((ref) {
   return RestClient(ref.watch(dioInstanceProvider));
+});
+final secureStorageProvider = Provider<SecureStorage>(
+  (ref) => SecureStorage(),
+);
+
+final userDaoProvider = Provider<UserDetailsDao>((ref) {
+  return UserDetailsDao(
+    ref.watch(secureStorageProvider),
+  );
 });
