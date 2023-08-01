e01c6ea425d2396fa104f8fd37489316ec427a92
Manil Pun <punmanil3@gmail.com>
2023-08-01 12:36:07 +0545

    project initialized

---
+++ android/app/src/main/kotlin/com/example/project_management_web_and_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_management_web_and_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/auth/login_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+              maxWidth: MediaQuery.sizeOf(context).width * 0.8,
+              maxHeight: MediaQuery.sizeOf(context).height * 0.8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.blue,
+                height: double.infinity,
+                width: MediaQuery.sizeOf(context).width * 0.4,
+              ),
+              Container(
+                height: double.infinity,
+                width: MediaQuery.sizeOf(context).width * 0.4,
+                color: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
