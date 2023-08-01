7f0303cc4280b4f03fbb941de731ca60f804c689
Manil Pun <punmanil3@gmail.com>
2023-08-01 13:11:21 +0545

    app theme textstyles

---
+++ lib/app/theme/text_styles.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/widgets.dart';
+
+/// Defines the text styles for the app.
+class AppTextStyle {
+  ///Bold Text 20sp
+  static TextStyle boldText20 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  );
+
+  ///Bold Text 16sp
+  static TextStyle boldText16 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+  );
+
+  ///Bold Text 14sp
+  static TextStyle boldText14 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+  );
+  static TextStyle boldText12 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+  );
+
+  ///SemiBold Text 18sp
+  static TextStyle semiBoldText20 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  );
+
+  ///SemiBold Text 18sp
+  static TextStyle semiBoldText18 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+  );
+
+  ///SemiBold Text 16sp
+  static TextStyle semiBoldText16 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+
+  ///SemiBold Text 14sp
+  static TextStyle semiBoldText14 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+  );
+
+  ///SemiBold Text 12sp
+  static TextStyle semiBoldText12 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+  );
+
+  ///Medium Text 20sp
+  static TextStyle mediumText20 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+  );
+  static TextStyle mediumText30 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 30,
+  );
+
+  ///Medium Text 18sp
+  static TextStyle mediumText18 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+  );
+
+  ///Medium Text 19sp
+  static TextStyle mediumText19 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 19,
+  );
+
+  ///Medium Text 16sp
+  static TextStyle mediumText16 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+
+  ///Medium Text 14sp
+  static TextStyle mediumText14 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+
+  ///Medium Text 12sp
+  static TextStyle mediumText12 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+  );
+
+  static TextStyle mediumText11 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 11,
+  );
+  static TextStyle mediumText13 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w500,
+    fontSize: 13,
+  );
+
+  ///Medium Text 40sp
+  static TextStyle regularText40 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 40,
+    letterSpacing: 1,
+    height: 1.2,
+  );
+
+  ///Medium Text 60sp
+  static TextStyle regularText60 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 60,
+    letterSpacing: 1,
+    height: 1.2,
+  );
+
+  static TextStyle regularText24 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 24,
+    letterSpacing: 1,
+    height: 1.2,
+  );
+
+  static TextStyle regularText20 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+  );
+
+  ///Medium Text 18sp
+  static TextStyle regularText18 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 18,
+  );
+
+  ///Medium Text 14sp
+  static TextStyle regularText16 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+
+  ///Medium Text 14sp
+  static TextStyle regularText14 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+  static TextStyle regularText15 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 15,
+  );
+  static TextStyle regularText13 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+  );
+
+  ///Medium Text 12sp
+  static TextStyle regularText12 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+
+  ///Medium Text 10sp
+  static TextStyle regularText10 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+  );
+
+  static TextStyle regularText11 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w400,
+    fontSize: 11,
+  );
+
+  ///Medium Text 12sp
+  static TextStyle lightText12 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w300,
+    fontSize: 12,
+  );
+
+  static TextStyle lightText14 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w300,
+    fontSize: 14,
+  );
+  static TextStyle lightText13 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w300,
+    fontSize: 13,
+  );
+
+  static TextStyle lightText15 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w300,
+    fontSize: 14,
+  );
+
+  static TextStyle lightText10 = const TextStyle(
+    fontFamily: 'PoppinsRegular',
+    fontWeight: FontWeight.w300,
+    fontSize: 10,
+  );
+}
