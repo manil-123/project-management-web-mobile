7e8baccc1f3537bb3f271f07da508ba0744ebc82
Manil Pun <punmanil3@gmail.com>
2023-08-04 16:30:58 +0545

    register

--- lib/feature/auth/repository/auth_repository.dart
+++ lib/feature/auth/repository/auth_repository.dart
@@ -7,6 +7,7 @@ import 'package:project_management_web_and_mobile/app/typedef/typedef.dart';
 import 'dart:developer';
 
 abstract class AuthRepository {
+  GetAuthResponse register(String username, String password);
   GetAuthResponse login(String username, String password);
 }
 
@@ -14,6 +15,31 @@ class AuthRepositoryImpl implements AuthRepository {
   AuthRepositoryImpl({required RestClient restClient})
       : _restClient = restClient;
   final RestClient _restClient;
+
+  @override
+  GetAuthResponse register(String username, String password) {
+    return runApiZoned(
+      fn: () async {
+        final response =
+            await _restClient.login(username: username, password: password);
+        log("response $response");
+        if (response.success == true) {
+          return Right(response);
+        } else {
+          return Left(Failure(
+            errorMessage: response.message,
+          ));
+        }
+      },
+      exceptionHandler: (error) {
+        return Left(
+          Failure(
+            errorMessage: NetworkExceptions.handleErrorToString(error),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   GetAuthResponse login(String username, String password) {
