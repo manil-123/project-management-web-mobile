a7a976104ecdc7f67a0a4193132cfb8ff8fb28b6
Manil Pun <punmanil3@gmail.com>
2023-08-08 13:06:53 +0545

    auth service modified

--- lib/utils/service/auth_service.dart
+++ lib/utils/service/auth_service.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:project_management_web_and_mobile/app/database/login_dao.dart';
+import 'package:project_management_web_and_mobile/providers/dependency_providers.dart';
 
 class AuthService extends ChangeNotifier {
-  bool _authenticated = false;
+  final LoginDao loginDao;
 
-  bool get authenticated => _authenticated;
+  AuthService(this.loginDao);
 
-  set authenticated(bool value) {
-    _authenticated = value;
+  Future<bool> get authStatus async => await loginDao.getLoginInfo() ?? false;
+
+  Future<void> saveAuthStatus(bool value) async {
+    await loginDao.saveLoginInfo(value);
     notifyListeners();
   }
 }
+
+final authServiceProvider = Provider<AuthService>((ref) {
+  return AuthService(
+    ref.watch(loginDaoProvider),
+  );
+});
