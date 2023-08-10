3185fce307cdeae85f300fc55c99b36decbb9bbf
Manil Pun <punmanil3@gmail.com>
2023-08-10 17:02:26 +0545

    dashboard providers

---
+++ lib/feature/dashboard/provider/dashboard_provider.dart
@@ -0,0 +1,39 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:project_management_web_and_mobile/app/state/generic_state.dart';
+import 'package:project_management_web_and_mobile/feature/dashboard/model/dashboard_info.dart';
+import 'package:project_management_web_and_mobile/feature/dashboard/repository/dashboard_repository.dart';
+import 'package:project_management_web_and_mobile/providers/repository_providers.dart';
+
+final dashboardProvider = StateNotifierProvider.autoDispose<DashboardNotifier,
+    GenericState<GenericState<DashboardInfo>>>(
+  (ref) => DashboardNotifier(
+    ref.watch(dashboardRepositoryProvider),
+  ),
+);
+
+class DashboardNotifier extends StateNotifier<GenericState<DashboardInfo>> {
+  DashboardNotifier(
+    this._dashboardRepository,
+  ) : super(
+          const GenericState.initial(),
+        );
+
+  final DashboardRepository _dashboardRepository;
+
+  Future<void> register() async {
+    state = const GenericState.loading();
+    final result = await _dashboardRepository.getDashboardInfo();
+    result.fold(
+      (failure) {
+        state = GenericState.error(
+          failure.errorMessage,
+        );
+      },
+      (dashboardInfoResponse) async {
+        state = GenericState.success(
+          dashboardInfoResponse.data,
+        );
+      },
+    );
+  }
+}
--- lib/providers/repository_providers.dart
+++ lib/providers/repository_providers.dart
@@ -1,9 +1,16 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:project_management_web_and_mobile/feature/auth/repository/auth_repository.dart';
+import 'package:project_management_web_and_mobile/feature/dashboard/repository/dashboard_repository.dart';
 import 'package:project_management_web_and_mobile/providers/dependency_providers.dart';
 
 final authRepositoryProvider = Provider<AuthRepository>((ref) {
   return AuthRepositoryImpl(
+    restClient: ref.watch(restClientProvider),
+  );
+});
+
+final dashboardRepositoryProvider = Provider<DashboardRepository>((ref) {
+  return DashboardRepositoryImpl(
     restClient: ref.watch(restClientProvider),
   );
 });
