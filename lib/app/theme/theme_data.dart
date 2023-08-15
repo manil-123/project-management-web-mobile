dbdab667dc17bf123d31cc0c97667889d2217619
Manil Pun <punmanil3@gmail.com>
2023-08-15 13:10:24 +0545

    removed dart page transition for web

--- lib/app/theme/theme_data.dart
+++ lib/app/theme/theme_data.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:project_management_web_and_mobile/app/constants/app_colors.dart';
 import 'package:project_management_web_and_mobile/app/theme/text_styles.dart';
+import 'package:project_management_web_and_mobile/utils/custom_class/no_transition_builder.dart';
 
 ThemeData get appTheme => ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -12,4 +14,15 @@ ThemeData get appTheme => ThemeData(
         ),
       ),
       useMaterial3: true,
+      pageTransitionsTheme: PageTransitionsTheme(
+        builders: kIsWeb
+            ? {
+                for (final platform in TargetPlatform.values)
+                  platform: const NoTransitionsBuilder(),
+              }
+            : {
+                TargetPlatform.iOS: const CupertinoPageTransitionsBuilder(),
+                TargetPlatform.android: const ZoomPageTransitionsBuilder(),
+              },
+      ),
     );
