4144d06ca5751565b4e3f14834fcc71cac57de07
Manil Pun <punmanil3@gmail.com>
2023-08-15 12:27:15 +0545

    scroll issue on login widget

--- lib/feature/auth/view/login_signup_widget.dart
+++ lib/feature/auth/view/login_signup_widget.dart
@@ -60,141 +60,143 @@ class LoginSignUpWidget extends HookConsumerWidget {
       );
     });
 
-    return Form(
-      key: formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: 'Hey, hello',
-                  style: AppTextStyle.boldText20.copyWith(
-                    color: Colors.black,
-                  ),
-                ),
-                TextSpan(
-                  text: ' 👋',
-                  style: AppTextStyle.boldText20.copyWith(
-                    color: const Color(0xFFFFD04E),
-                  ),
-                ),
-              ],
-            ),
-          ).pB(10),
-          Text(
-            isLogin.value
-                ? 'Enter the information you entered while registering.'
-                : 'Enter the information for registration.',
-            style: AppTextStyle.regularText12.copyWith(
-              color: Colors.black.withOpacity(0.5),
-            ),
-          ).pB(40),
-          Text(
-            'Email',
-            style: AppTextStyle.boldText16,
-          ),
-          CustomTextFormField(
-            controller: emailController,
-            prefixIcon: Icons.person,
-            keyboardType: TextInputType.emailAddress,
-            validator: (name) {
-              if (name!.isEmpty) {
-                return 'Username cannot be empty';
-              }
-              return null;
-            },
-          ).pB(20),
-          Text(
-            'Password',
-            style: AppTextStyle.boldText16,
-          ),
-          CustomTextFormField(
-            controller: passwordController,
-            prefixIcon: Icons.lock,
-            keyboardType: TextInputType.visiblePassword,
-            obsecure: !isPasswordVisible.value,
-            validator: (password) {
-              if (password!.isEmpty) {
-                return 'Password cannot be empty';
-              }
-              return null;
-            },
-            suffixIcon: isPasswordVisible.value
-                ? Icons.visibility
-                : Icons.visibility_off,
-            onSufficIconPress: () {
-              isPasswordVisible.value = !isPasswordVisible.value;
-            },
-          ).pB(30),
-          InkWell(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                if (isLogin.value) {
-                  ref.read(authProvider.notifier).login(
-                        emailController.text.trim(),
-                        passwordController.text.trim(),
-                      );
-                } else {
-                  ref.read(authProvider.notifier).register(
-                        emailController.text.trim(),
-                        passwordController.text.trim(),
-                      );
-                }
-              } else {
-                shakeButton.value = true;
-                Future.delayed(const Duration(milliseconds: 1500), () {
-                  shakeButton.value = false;
-                });
-              }
-            },
-            child: shakeButton.value
-                ? LoginSignUpButton(
-                    loginModel: loginModel,
-                    isLogin: isLogin,
-                    shouldShakeButton: true,
-                  ).withShakeAnimation(
-                    delay: const Duration(milliseconds: 500),
-                    duration: const Duration(milliseconds: 1500),
-                    offset: const Offset(10.0, 0.0),
-                    shakes: 14.0,
-                  )
-                : LoginSignUpButton(
-                    loginModel: loginModel,
-                    isLogin: isLogin,
-                    shouldShakeButton: false,
-                  ),
-          ).pB(30),
-          Center(
-            child: RichText(
+    return SingleChildScrollView(
+      child: Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            RichText(
               text: TextSpan(
                 children: [
                   TextSpan(
-                    text: isLogin.value
-                        ? 'Don\'t have account?'
-                        : 'Already have account?',
-                    style: AppTextStyle.semiBoldText12.copyWith(
-                      color: Colors.black.withOpacity(0.5),
+                    text: 'Hey, hello',
+                    style: AppTextStyle.boldText20.copyWith(
+                      color: Colors.black,
                     ),
                   ),
                   TextSpan(
-                    text: isLogin.value ? ' Sign Up' : ' Login',
-                    style: AppTextStyle.semiBoldText12.copyWith(
-                      color: Colors.black,
+                    text: ' 👋',
+                    style: AppTextStyle.boldText20.copyWith(
+                      color: const Color(0xFFFFD04E),
                     ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        isLogin.value = !isLogin.value;
-                      },
                   ),
                 ],
               ),
             ).pB(10),
-          ),
-        ],
-      ).pXY(contentPadding, 30),
+            Text(
+              isLogin.value
+                  ? 'Enter the information you entered while registering.'
+                  : 'Enter the information for registration.',
+              style: AppTextStyle.regularText12.copyWith(
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ).pB(40),
+            Text(
+              'Email',
+              style: AppTextStyle.boldText16,
+            ),
+            CustomTextFormField(
+              controller: emailController,
+              prefixIcon: Icons.person,
+              keyboardType: TextInputType.emailAddress,
+              validator: (name) {
+                if (name!.isEmpty) {
+                  return 'Username cannot be empty';
+                }
+                return null;
+              },
+            ).pB(20),
+            Text(
+              'Password',
+              style: AppTextStyle.boldText16,
+            ),
+            CustomTextFormField(
+              controller: passwordController,
+              prefixIcon: Icons.lock,
+              keyboardType: TextInputType.visiblePassword,
+              obsecure: !isPasswordVisible.value,
+              validator: (password) {
+                if (password!.isEmpty) {
+                  return 'Password cannot be empty';
+                }
+                return null;
+              },
+              suffixIcon: isPasswordVisible.value
+                  ? Icons.visibility
+                  : Icons.visibility_off,
+              onSufficIconPress: () {
+                isPasswordVisible.value = !isPasswordVisible.value;
+              },
+            ).pB(30),
+            InkWell(
+              onTap: () {
+                if (formKey.currentState!.validate()) {
+                  if (isLogin.value) {
+                    ref.read(authProvider.notifier).login(
+                          emailController.text.trim(),
+                          passwordController.text.trim(),
+                        );
+                  } else {
+                    ref.read(authProvider.notifier).register(
+                          emailController.text.trim(),
+                          passwordController.text.trim(),
+                        );
+                  }
+                } else {
+                  shakeButton.value = true;
+                  Future.delayed(const Duration(milliseconds: 1500), () {
+                    shakeButton.value = false;
+                  });
+                }
+              },
+              child: shakeButton.value
+                  ? LoginSignUpButton(
+                      loginModel: loginModel,
+                      isLogin: isLogin,
+                      shouldShakeButton: true,
+                    ).withShakeAnimation(
+                      delay: const Duration(milliseconds: 500),
+                      duration: const Duration(milliseconds: 1500),
+                      offset: const Offset(10.0, 0.0),
+                      shakes: 14.0,
+                    )
+                  : LoginSignUpButton(
+                      loginModel: loginModel,
+                      isLogin: isLogin,
+                      shouldShakeButton: false,
+                    ),
+            ).pB(30),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: isLogin.value
+                          ? 'Don\'t have account?'
+                          : 'Already have account?',
+                      style: AppTextStyle.semiBoldText12.copyWith(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                    TextSpan(
+                      text: isLogin.value ? ' Sign Up' : ' Login',
+                      style: AppTextStyle.semiBoldText12.copyWith(
+                        color: Colors.black,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          isLogin.value = !isLogin.value;
+                        },
+                    ),
+                  ],
+                ),
+              ).pB(10),
+            ),
+          ],
+        ).pXY(contentPadding, 30),
+      ),
     );
   }
 }
