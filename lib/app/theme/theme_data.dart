739dc337c9e2537110fbd59fc81cbe912bde1827
Manil Pun <punmanil3@gmail.com>
2023-08-21 12:12:00 +0545

    change password view

--- lib/app/theme/theme_data.dart
+++ lib/app/theme/theme_data.dart
@@ -5,12 +5,27 @@ import 'package:project_management_web_and_mobile/app/theme/text_styles.dart';
 import 'package:project_management_web_and_mobile/utils/custom_class/no_transition_builder.dart';
 
 ThemeData get appTheme => ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       appBarTheme: AppBarTheme(
         backgroundColor: AppColors.kWebLightColor,
         iconTheme: const IconThemeData(color: Colors.white),
         titleTextStyle: AppTextStyle.boldText14.copyWith(
           color: Colors.white,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.kWebDarkColor,
+          padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+          elevation: 0,
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6.0),
+          ),
         ),
       ),
       useMaterial3: true,
--- lib/feature/settings/view/change_password_screen.dart
+++ lib/feature/settings/view/change_password_screen.dart
@@ -1,10 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:project_management_web_and_mobile/app/theme/text_styles.dart';
+import 'package:project_management_web_and_mobile/app/widgets/custom_app_bar.dart';
+import 'package:project_management_web_and_mobile/app/widgets/custom_text_form_field.dart';
+import 'package:project_management_web_and_mobile/utils/extensions/padding_extension.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
-class ChangePasswordScreen extends StatelessWidget {
-  const ChangePasswordScreen({super.key});
+class ChangePasswordScreen extends HookConsumerWidget {
+  ChangePasswordScreen({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final oldPasswordController = useTextEditingController();
+    final newPasswordController = useTextEditingController();
+
+    final isOldPasswordVisible = useState<bool>(false);
+    final isNewPasswordVisible = useState<bool>(false);
+
+    var deviceType = getDeviceType(MediaQuery.sizeOf(context));
+
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: deviceType == DeviceScreenType.desktop
+                ? MediaQuery.sizeOf(context).width * .5
+                : MediaQuery.sizeOf(context).width * .8,
+          ),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Old Password',
+                    style: AppTextStyle.boldText16,
+                  ).pB(8),
+                  CustomTextFormField(
+                    controller: oldPasswordController,
+                    prefixIcon: Icons.lock,
+                    keyboardType: TextInputType.visiblePassword,
+                    obsecure: !isOldPasswordVisible.value,
+                    validator: (password) {
+                      if (password!.isEmpty) {
+                        return 'Old password cannot be empty';
+                      }
+                      return null;
+                    },
+                    suffixIcon: isOldPasswordVisible.value
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    onSufficIconPress: () {
+                      isOldPasswordVisible.value = !isOldPasswordVisible.value;
+                    },
+                  ).pB(40),
+                  Text(
+                    'New Password',
+                    style: AppTextStyle.boldText16,
+                  ).pB(8),
+                  CustomTextFormField(
+                    controller: newPasswordController,
+                    prefixIcon: Icons.lock,
+                    keyboardType: TextInputType.visiblePassword,
+                    obsecure: !isNewPasswordVisible.value,
+                    validator: (password) {
+                      if (password!.isEmpty) {
+                        return 'New password cannot be empty';
+                      }
+                      return null;
+                    },
+                    suffixIcon: isNewPasswordVisible.value
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    onSufficIconPress: () {
+                      isNewPasswordVisible.value = !isNewPasswordVisible.value;
+                    },
+                  ).pB(20),
+                  const SizedBox(
+                    height: 40.0,
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Change Password',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
